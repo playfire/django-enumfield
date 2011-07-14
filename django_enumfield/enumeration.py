53794891762b01992c134cd0f2a20362d370255c
Ben Phillips <ben@playfire.com>
2011-07-14 16:36:42 +0100

    Change the default ordering of items to be sorted by display instead of value.

--- django_enumfield/enumeration.py
+++ django_enumfield/enumeration.py
@@ -27,7 +27,7 @@ class EnumerationMeta(type):
 
                 items.append((n, item))
 
-        items.sort(key=lambda i: i[1].value)
+        items.sort(key=lambda i: i[1].display)
         item_objects = [i[1] for i in items]
 
         by_val = dict((i.value, i) for i in item_objects)
