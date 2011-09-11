3b6112587bfd6779df8862a97d263e8acb05a7ae
Chris Lamb <chris@playfire.com>
2011-09-11 05:39:32 +0100

    Could have sworn South accepting a slug previously.

--- django_enumfield/fields.py
+++ django_enumfield/fields.py
@@ -41,6 +41,6 @@ class EnumField(models.Field):
         # repr(Item) is not only invalid as an lookup value, it actually causes
         # South to generate invalid Python
         if self.default != NOT_PROVIDED:
-            kwargs['default'] = repr(self.default.slug)
+            kwargs['default'] = self.default.value
 
         return ('django.db.models.fields.IntegerField', args, kwargs)
